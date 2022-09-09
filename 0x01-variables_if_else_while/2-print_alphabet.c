298e81a4746fe488c9cc3d03694825e6fd6516dd
cleopasrotich <cleopasrotich81@gmail.com>
2022-09-09 15:29:25 +0300

    Printing small alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - To determine the alphabet in lower case
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar (i);
+}
+return (0);
+}
