9cf293be88b4fdec832720afe5015184b386c96f
cleopasrotich <cleopasrotich81@gmail.com>
2022-09-09 14:49:55 +0300

    to determine random no of var n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ n = rand() - RAND_MAX / 2;
 int n;
 char last[] = "Last digit of";
 
-printf("%s %d is %d", last, n, n % 10);
+printf("%s %d is %d and is", last, n, n % 10);
 if (n % 10 > 5)
 {
 printf("greater than 5\n");
@@ -28,7 +28,7 @@ printf("0\n");
 }
 else
 {
-printf("less than 6 and not 0\n");
+printf("less than 6 and not 0");
 }
 return (0);
 
