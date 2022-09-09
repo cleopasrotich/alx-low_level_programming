11dc289600b4c21bf2919b13c70cd9099427b289
cleopasrotich <cleopasrotich81@gmail.com>
2022-09-09 15:56:11 +0300

    Printing small alphabet and big

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - To determine the alphabet in both lower case and upper case
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+char i;
+char z;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+for (z = 'A'; z <= 'Z'; z++)
+{
+putchar(z);
+}
+putchar("\n");
+return (0);
+}
